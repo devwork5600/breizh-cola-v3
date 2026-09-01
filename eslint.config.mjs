9b117baa145544e189f6bcc987d00bfce07459b7
Adrien <devwork5600@gmail.com>
2026-09-01 13:21:06 +0200

    chore: setup CI, prettier, eslint, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,46 @@
+import nextPlugin from "@next/eslint-plugin-next";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginImport from "eslint-plugin-import";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierRecommended,
+  {
+    plugins: {
+      import: eslintPluginImport,
+      "@next": nextPlugin,
+    },
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+
+      // Import sorting rules
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin", // fs, path
+            "external", // react, next
+            "internal", // @/lib, @/components
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      // Rules from nextPlugin
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
