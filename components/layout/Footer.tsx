ca9e11a9986d2deb2eab62bafa0cdeb02206bf38
devwork5600 <devwork5600@gmail.com>
2026-09-01 20:28:07 +0200

    fix: restore reveal footer effect and z-index stacking (#12)

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,12 +1,25 @@
+import { Copyright } from "lucide-react";
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="bg-secondary text-primary flex h-25 w-full items-center justify-between px-4">
-      <span className="text-md font-semibold capitalize xl:text-xl">
-        &copy; {new Date().getFullYear()}
-      </span>
-      <span className="font-cream-cake text-right text-3xl capitalize sm:text-4xl 2xl:text-5xl">
-        Breizh Cola
-      </span>
-    </footer>
+    <div
+      className="bg-secondary text-primary relative z-30 h-25"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+    >
+      <div className="fixed bottom-0 flex h-25 w-full items-center justify-between px-4">
+        <div className="flex items-center">
+          <Copyright strokeWidth={1.5} />
+          <span className="text-md pl-1 font-semibold capitalize xl:text-xl">
+            copyright {currentYear}
+          </span>
+        </div>
+
+        <span className="font-cream-cake text-right text-3xl capitalize sm:text-4xl 2xl:text-5xl">
+          breizh cola
+        </span>
+      </div>
+    </div>
   );
 }
