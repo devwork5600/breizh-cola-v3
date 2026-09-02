7f49537e4cd5f3cd5075cb2690e14aa9955fa8d0
Adrien <devwork5600@gmail.com>
2026-09-02 13:30:55 +0200

    feat: mirror ProductLime' s desktop layout

--- app/(main)/(home)/components/productLime/ProductLime.tsx
+++ app/(main)/(home)/components/productLime/ProductLime.tsx
@@ -9,22 +9,22 @@ import Scene from "./Scene";
 
 export default function ProductLime() {
   return (
-    <div className="product-lime relative flex min-h-screen w-full justify-center overflow-hidden px-2 pt-16 lg:px-8 xl:items-center xl:justify-start xl:pt-0">
+    <div className="product-lime relative flex min-h-screen w-full justify-center overflow-hidden px-2 pt-16 lg:px-8 xl:items-center xl:justify-end xl:pt-0">
       <View className="pointer-events-none absolute top-0 left-0 z-30 h-full w-full">
         <Scene />
       </View>
-      <div className="absolute top-[84vh] left-[35%] sm:top-[57vh] sm:left-[55vw]">
+      <div className="absolute top-[84vh] right-[35%] sm:top-[57vh] sm:right-[55vw]">
         <MagneticButtons>
           <Link href="/product/lime">
-            <button className="text-secondary hover:bg-secondary hover:text-primary bg-primary relative h-22 w-44 rotate-8 cursor-pointer rounded-[50%] border-2 font-bold uppercase sm:h-28 sm:w-56 sm:text-xl md:left-0 md:h-31.25 md:w-62.5 md:text-2xl xl:h-37.5 xl:w-75 xl:text-4xl 2xl:h-45 2xl:w-90 2xl:-rotate-11">
+            <button className="text-secondary hover:bg-secondary hover:text-primary bg-primary relative h-22 w-44 rotate-8 cursor-pointer rounded-[50%] border-2 font-bold uppercase sm:h-28 sm:w-56 sm:text-xl md:right-0 md:h-31.25 md:w-62.5 md:text-2xl xl:h-37.5 xl:w-75 xl:text-4xl 2xl:h-45 2xl:w-90 2xl:-rotate-11">
               decouvrez-le
             </button>
           </Link>
         </MagneticButtons>
       </div>
 
-      <div className="font-poppins flex flex-col gap-3 text-[34px] tracking-wider uppercase sm:gap-8 sm:text-5xl lg:gap-10 lg:text-6xl xl:gap-12 xl:text-7xl 2xl:gap-18 2xl:text-[86px]">
-        <div className="flex flex-col sm:gap-3">
+      <div className="font-poppins flex flex-col gap-3 text-[34px] tracking-wider uppercase sm:gap-8 sm:text-5xl lg:gap-10 lg:text-6xl xl:items-end xl:gap-12 xl:text-7xl 2xl:gap-18 2xl:text-[86px]">
+        <div className="flex flex-col sm:gap-3 xl:items-end">
           <p className="flex gap-2">
             <span className="text-secondary">note</span>
             <span className="text-stroke-secondary-1 text-primary xl:tracking-wider">
@@ -38,7 +38,7 @@ export default function ProductLime() {
             <span className="text-secondary">verts</span>
           </p>
         </div>
-        <div className="flex flex-col sm:gap-3">
+        <div className="flex flex-col sm:gap-3 xl:items-end">
           <p className="flex gap-2">
             <span className="text-secondary">encore</span>
             <span className="text-stroke-secondary-1 text-primary xl:tracking-wider">
